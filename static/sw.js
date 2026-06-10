3d8e0cf48d5d3f7d8a9aacc13f03dfa6a83c3fb4
Alear2334 <160791861+Alear2334@users.noreply.github.com>
2026-06-09 20:29:15 -0400

    Agregando archivos PWA

--- static/sw.js
+++ static/sw.js
@@ -1,10 +1,5 @@
-const CACHE_NAME = 'bingo360-v1';
-const urlsToCache = ['/', '/static/css/estilos.css']; // Puedes añadir más archivos si quieres
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
